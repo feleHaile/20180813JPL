222ff6f56a251ffc9aedb81efb9e8d6cdb63b3e0
jstrickler <jstrickler@gmail.com>
2018-08-13 17:00:23 -0700

    Classes

--- carddeck.py
+++ carddeck.py
@@ -1,1 +1,55 @@
 #!/usr/bin/env python
+import random
+
+class CardDeck():
+    SUITS = 'C D H S'.split()
+    RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
+
+    def __init__(self, dealer):
+        self._dealer = None
+        self._cards = None
+
+        self.dealer = dealer
+
+        self._create_deck()
+
+    def _create_deck(self):
+        self._cards = []
+        for s in self.SUITS:
+            for r in self.RANKS:
+                card = r + s
+                self._cards.append(card)
+
+
+    # getter property
+    @property
+    def dealer(self):
+        return self._dealer
+
+    # setter property
+    @dealer.setter
+    def dealer(self, dealer):
+        if isinstance(dealer, str):
+            self._dealer = dealer
+        else:
+            raise TypeError("Dealer must be a string")
+
+
+    @property
+    def cards(self):
+        return self._cards
+
+
+    def shuffle(self):
+        random.shuffle(self._cards)
+
+    def draw(self):
+        return self._cards.pop()
+
+    @classmethod
+    def get_ranks(cls):
+        return cls.RANKS
+
+    @staticmethod
+    def beep():
+        print("Beep! ")
--- play_cards.py
+++ play_cards.py
@@ -1,19 +1,50 @@
 #!/usr/bin/env python
 
 from carddeck import CardDeck
+from jokerdeck import JokerDeck
 
-d = CardDeck()
+d1 = CardDeck('Beverly')
+d2 = CardDeck("Manuelo")
 
+print(d1)
+print(d1.dealer)
 
-# d1 = CardDeck('Beverly')
-# d2 = CardDeck("Manuelo")
-#
-# d1.shuffle()
-#
-# hand = []
-# for i in range(5):
-#     hand.append(d1.draw())
-# print("Hand:", hand)
-#
-# print(d1.dealer)
-# print(d1.cards)
+d1.dealer = 'Steve'
+
+print(d1.dealer)
+
+try:
+    d1.dealer = ['a', 5, 99]
+except TypeError as err:
+    print(err)
+else:
+    print(d1.dealer)
+
+d1.shuffle()
+print(d1.cards)
+
+d1.shuffle()
+
+hand = []
+for i in range(5):
+    hand.append(d1.draw())
+print("Hand:", hand)
+
+print(d1.get_ranks())
+print(CardDeck.get_ranks())
+
+d1.beep()
+d1.beep()
+
+CardDeck.beep()
+print('-' * 60)
+j1 = JokerDeck('Albert')
+j1.shuffle()
+
+print(j1.draw())
+print(j1.cards)
+
+j1.bark()
+
+print(JokerDeck.mro())
+
